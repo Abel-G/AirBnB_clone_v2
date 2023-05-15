bee4185884a2dfbc9e7be366e4b8000d8d2570b4
Abel-G <abeljeb@gmail.com>
2023-05-15 10:03:18 -0700

    print

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -4,7 +4,7 @@
 -- Grant SELECT privilege on the database performance_schema to the user hbnb_test
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
