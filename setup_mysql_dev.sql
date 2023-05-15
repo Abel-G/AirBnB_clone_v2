d258f4a7f49323de5a84e4c02c2fd9a9cbc0a5c1
Abel-G <abeljeb@gmail.com>
2023-05-15 09:44:11 -0700

    retouch

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,8 +4,7 @@
 -- Grant all privileges on the database hbnb_dev_db to the user hbnb_dev
 -- Grant SELECT privilege on the database performance_schema to the user hbnb_dev
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
-SET PASSWORD FOR 'hbnb_dev'@'localhost' = PASSWORD('hbnb_dev_pwd');
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 FLUSH PRIVILEGES;
